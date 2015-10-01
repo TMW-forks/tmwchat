385a3e61c8aab7e9a8524d66c5cd0124e2456375
Joseph Botosh <rumly111@gmail.com>
2015-10-01 17:24:59 +0300

    more code refactoring

--- tmwchat-util.el
+++ tmwchat-util.el
@@ -1,9 +1,14 @@
 (require 'tmwchat-log)
 
 (defun tmwchat-decode-string (s)
-  ;; (condition-case nil
-      (decode-coding-string s 'utf-8 t))
-    ;; (error "{{STRING_DECODE_ERROR}}")))
+  (condition-case nil
+      (decode-coding-string s 'utf-8 t)
+    (error "{{STRING_DECODE_ERROR}}")))
+
+(defun tmwchat--contains-302-202 (str)
+  "Check if string contains ManaPlus-specific messages with \302\202
+   that breaks utf8 decoding"
+  (string-match-p "^#o302|#o202" str))
 
 (defun vec-less (v1 v2)
   (or (< (elt v1 3) (elt v2 3))
@@ -28,5 +33,26 @@
 		 -4))
     (cons x y)))
 (make-variable-buffer-local 'tmwchat-read-coordinates)
+
+(defun tmwchat-escape-percent (str)
+  (let ((splt (split-string str "%")))
+    (mapconcat 'identity splt "%%")))
+
+(defun tmwchat--remove-color (str)
+  (while (string-match "##[0-9bB]" str)
+    (setq str (replace-match "" nil nil str)))
+  (ignore-errors
+    (let ((beg) (code) (emote))
+      (while (setq beg (string-match "%%[^%]" str))
+	(setq code (- (elt str (+ beg 2)) 48)
+	      emote (elt tmwchat-emotes-2 code)
+	      str (replace-match emote nil nil str)))))
+  (setq str (tmwchat-escape-percent str))
+  str)
+
+(defun tmwchat-make-read-only ()
+  "Make all the text in the current buffer read-only."
+  (add-text-properties (point-min) tmwchat--start-point
+		       '(read-only t front-sticky t rear-nonsticky t)))
 
 (provide 'tmwchat-util)
--- tmwchat.el
+++ tmwchat.el
@@ -6,6 +6,7 @@
 (require 'tmwchat-charsrv)
 (require 'tmwchat-mapserv)
 (require 'tmwchat-speedbar)
+(require 'tmwchat-input)
 (require 'tmwchat-log)
 
 ;;------------------------------------------------------------------
@@ -151,7 +152,7 @@
 
 (defvar tmwchat-away nil "User is away if value is t")
 
-(setq tmwchat--client-process nil)
+(defvar tmwchat--client-process nil)
 
 (defvar tmwchat--adding-being-ids nil
   "Set of IDs that are being requested from server")
@@ -164,6 +165,7 @@
   "Current map name")
 
 
+;;===================================================================
 (defun tmwchat--cleanup ()
   (when (timerp tmwchat-ping-mapserv-timer)
     (cancel-timer tmwchat-ping-mapserv-timer))
@@ -195,89 +197,7 @@
       (tmwchat-equip-item next-id)))))
 
 
-
-
-
-;;-------------------------------------------------------------------
-
-
-
-
-;;====================================================================
-(defun tmwchat-show-beings ()
-  (dolist (id tmwchat-nearby-player-ids)
-    (let ((name (tmwchat-being-name id)))
-      (tmwchat-log (format "%s (id:%s)" name id)))))
-
-(defun tmwchat--notify-filter (msg)
-  (let ((regex (mapconcat
-		'identity
-		(mapcar 'regexp-quote tmwchat-notify-words)
-		"\\|")))
-    (not (eq (string-match-p regex msg) nil))))
-
-(defun tmwchat--notify (title text)
-  ;; (message (format "frame-visible: %s" (frame-visible-p (selected-frame))))
-  (unless (and (eq (frame-visible-p (selected-frame)) t)
-	       (get-buffer-window (process-buffer
-				   (get-process "tmwchat"))
-				  'visible))
-    (let ((icon (concat (file-name-as-directory tmwchat-root-directory)
-			"Tmw_logo.png"))
-	  (sound (concat (file-name-as-directory tmwchat-root-directory)
-			 "newmessage.wav")))
-      (todochiku-message title text icon tmwchat-sticky-notifications)
-      (when tmwchat-sound
-	(play-sound-file sound)))))
-
-(defun tmwchat-escape-percent (str)
-  (let ((splt (split-string str "%")))
-    (mapconcat 'identity splt "%%")))
-
-(defun tmwchat--remove-color (str)
-  (while (string-match "##[0-9bB]" str)
-    (setq str (replace-match "" nil nil str)))
-  (ignore-errors
-    (let ((beg) (code) (emote))
-      (while (setq beg (string-match "%%[^%]" str))
-	(setq code (- (elt str (+ beg 2)) 48)
-	      emote (elt tmwchat-emotes-2 code)
-	      str (replace-match emote nil nil str)))))
-  (setq str (tmwchat-escape-percent str))
-  str)
-
-(defun tmwchat--replace-whisper-cmd (nick)
-  (interactive "sNick:")
-  (defun insert-formatted (nick-q msg)
-    (with-current-buffer tmwchat-buffer-name
-      (delete-region tmwchat--start-point (point-max))
-      (insert (concat "/w " nick-q " " msg))
-      (goto-char (point-max))))
-  (with-current-buffer tmwchat-buffer-name
-    (let ((line (buffer-substring tmwchat--start-point (point-max)))
-	  (nick-q (if (string-match-p " " nick)
-		      (concat "\"" nick "\"")
-		    nick)))
-      (condition-case nil
-	  (if (string-prefix-p "/w " line)
-	      (let* ((parsed (tmwchat--parse-msg (substring line 3)))
-		     (old-nick (car parsed))
-		     (msg (cdr parsed)))
-		(unless (string-equal old-nick nick)
-		  (insert-formatted nick-q msg)))
-	    (insert-formatted nick-q ""))
-	(error (insert-formatted nick-q ""))))))
-
-(defun tmwchat--whisper-to-buffer (nick msg)
-  (with-current-buffer (get-buffer-create (concat "TMW: " nick))
-    (tmwchat-mode)
-    (setq-local tmwchat--whisper-target nick)
-    (goto-char (point-max))
-    ;; if not curr_buffer unread[nick]++
-    (insert msg)
-    (newline)))
-
-;;----------------------------------------------------------------------
+;;=================================================================
 (defun tmwchat-insert-url (href label)
   "Insert a clickable URL at current position"
   (interactive "sUrl:\nsLabel:")
@@ -322,67 +242,102 @@
 	    str (replace-match m+url t t str 1)))
     (substring str 1)))
 
-(defun tmwchat--contains-302-202 (str)
-  "Check if string contains ManaPlus-specific messages with \302\202
-   that breaks utf8 decoding"
-  (string-match-p "^#o302|#o202" str))
-
-;;----------------------------------------------------------------------
-(defun tmwchat--find-nick-completion ()
-  (defun completion-list ()
-    (union
-     (tmwchat-get-online-users)
-     (ring-elements tmwchat-recent-users)))
-  (defun filter (condp lst)
-    (delq nil
-	  (mapcar (lambda (x) (and (funcall condp x) x)) lst)))
-  (let ((onl (tmwchat-get-online-users))
-	(len 3)
-	(partial)
-	(nick)
-	(memb))
-    (while (> (length onl) 1)
-      (setq partial (buffer-substring (- (point) len) (point)))
-      (setq onl (filter (lambda (nick)
-			  (string-match-p (regexp-quote partial) nick))
-			onl))
-      (when (setq memb (member-ignore-case partial onl))
-	(setq onl (list (car memb))))
-      (setq len (1+ len)))
-    (when onl
-      (setq len (- len 1))
-      (setq nick (car onl))
-      (while (string-match-p (regexp-quote partial) nick)
-	(setq len (1+ len))
-	(setq partial (buffer-substring (- (point) len) (point))))
-      (setq len (- len 1))
-      (when (eq (aref partial 1) 32)
-	(setq len (- len 1)))
-      (cons nick len))))
-
-(defun tmwchat--replace-nick-completion (nick partial-len)
-  (let* ((pt-end (point))
-	 (pt-begin (- pt-end partial-len))
-	 (nick-q (if (string-match-p " " nick)
-		     (concat "\"" nick "\" ")
-		   (concat nick " "))))
-    (when (equal (char-before pt-begin) 34)
-      (setq pt-begin (- pt-begin 1)))
-    (delete-region pt-begin pt-end)
-    (insert nick-q)))
- 
-(defun tmwchat-tab-complete ()
-  (interactive)
-  (let ((result (tmwchat--find-nick-completion)))
-    (when result
-      (tmwchat--replace-nick-completion (car result) (cdr result)))))
 
 ;;====================================================================
-(defun tmwchat-make-read-only ()
-  "Make all the text in the current buffer read-only."
-  (add-text-properties (point-min) tmwchat--start-point
-		       '(read-only t front-sticky t rear-nonsticky t)))
+(defun tmwchat--notify-filter (msg)
+  (let ((regex (mapconcat
+		'identity
+		(mapcar 'regexp-quote tmwchat-notify-words)
+		"\\|")))
+    (not (eq (string-match-p regex msg) nil))))
 
+(defun tmwchat--notify (title text)
+  ;; (message (format "frame-visible: %s" (frame-visible-p (selected-frame))))
+  (unless (and (eq (frame-visible-p (selected-frame)) t)
+	       (get-buffer-window (process-buffer
+				   (get-process "tmwchat"))
+				  'visible))
+    (let ((icon (concat (file-name-as-directory tmwchat-root-directory)
+			"Tmw_logo.png"))
+	  (sound (concat (file-name-as-directory tmwchat-root-directory)
+			 "newmessage.wav")))
+      (todochiku-message title text icon tmwchat-sticky-notifications)
+      (when tmwchat-sound
+	(play-sound-file sound)))))
+
+
+;;====================================================================
+(defun tmwchat-set-online-users (users-list)
+  (cond
+   ((= tmwchat--online-list-number 0)
+    (setq tmwchat--online-list-number 1
+	  tmwchat--online-list-0 users-list))
+   ((= tmwchat--online-list-number 1)
+    (setq tmwchat--online-list-number 0
+	  tmwchat--online-list-1 users-list))))
+
+(defun tmwchat-get-online-users ()
+  (cond
+   ((= tmwchat--online-list-number 0)
+    tmwchat--online-list-1)
+   ((= tmwchat--online-list-number 1)
+    tmwchat--online-list-0)))
+
+(defun tmwchat--online-list ()
+  (defun chomp-end (str)
+    (when (string-suffix-p "(GM) " str)
+      (setq str (substring str 0 -5)))
+    (replace-regexp-in-string (rx (* (any " \t\n")) eos)
+			      ""
+			      str))
+  (defun gen-list (str)
+    (let* ((m (string-match "------------------------------" str))
+	   (end (+ (match-end 0) 1))
+	   (strm (substring str end))
+	   (m2 (string-match "\n\n" strm))
+	   (start (match-beginning 0))
+	   (strmm (substring strm 0 start)))
+      (mapcar 'chomp-end (split-string strmm "\n"))))
+  (defun callback (status)
+    (let ((onl)
+	  (data (buffer-string)))
+      (setq onl (gen-list data))
+      (tmwchat-set-online-users onl)
+      (setq tmwchat--speedbar-dirty t)
+      (kill-buffer (current-buffer))))
+  (let ((url "http://server.themanaworld.org/online.txt"))
+    (url-retrieve url 'callback nil t t)))
+
+
+;;====================================================================
+(defun tmwchat-time ()
+  (let ((date (format-time-string "%D")))
+    (if (string-equal date tmwchat--date)
+	(format-time-string "%R")
+      (progn
+	(setq tmwchat--date date)
+	(format-time-string "%D %R")))))
+
+(defun tmwchat-redisplay-player-name (id name)
+  (with-current-buffer tmwchat-buffer-name
+    (save-excursion
+      (let ((inhibit-read-only t)
+	    (old-name (format "{{ID:%s}}" id)))
+	(replace-string old-name name nil (point-min) (point-max))
+	(goto-char (point-max))
+	(beginning-of-line)
+	(setq tmwchat--start-point (point))))))
+
+(defun tmwchat--whisper-to-buffer (nick msg)
+  (with-current-buffer (get-buffer-create (concat "TMW: " nick))
+    (tmwchat-mode)
+    (setq-local tmwchat--whisper-target nick)
+    (goto-char (point-max))
+    ;; if not curr_buffer unread[nick]++
+    (insert msg)
+    (newline)))
+
+;;====================================================================
 (defvar tmwchat-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map "\r" 'tmwchat-read-print)
@@ -435,199 +390,6 @@
   (setq tmwchat--window (selected-window))
   (tmwchat-login tmwchat-server-host tmwchat-server-port))
 
-(defun tmwchat-read-print ()
-  "Top level loop."
-  (interactive)
-  (unless (equal tmwchat--start-point (point))
-    (setq tmwchat-sent (tmwchat-readin))
-    (newline)
-    ;; (setq tmwchat--start-point (point))
-    (tmwchat-parse-input)
-    ;; (newline)
-    ))
 
-(defun tmwchat-readin ()
-  "Read message and return it"
-  (goto-char (point-max))
-  (buffer-substring tmwchat--start-point (point)))
-
-(defun tmwchat--parse-msg (msg)
-  (unless (and (stringp msg) (> (length msg) 0))
-    (error "tmwch0at--parse-msg: msg must be non-empty string"))
-  (if (string-match "^\"" msg)
-      (if (string-match "\"" (substring msg 1))
-	  (cons (substring msg 1 (match-end 0))
-		(substring msg (+ (match-end 0) 2)))
-	(error "Bad string format"))
-    (let ((m (string-match " " msg)))
-      (cons (substring msg 0 m)
-	    (substring msg (+ m 1))))))
-
-(defun chomp (str)
-  "Chomp leading and tailing whitespace from STR."
-  (replace-regexp-in-string (rx (or (: bos (* (any " \t\n")))
-				    (: (* (any " \t\n")) eos)))
-			    ""
-			    str))
-
-(defun tmwchat-parse-input ()
-  (cond
-   ((string-equal tmwchat-sent "/help")
-    (tmwchat-log
-     (concat
-      "/help -- show this help\n"
-      "/room -- show nearby players\n"
-      "/emote <number> -- show emote\n"
-      "/emotes -- show emote codes\n"
-      "/mute -- mute notification sounds\n"
-      "/unmute -- play notification sounds\n"
-      "/w NickName Message -- send a PM to NickName\n"
-      "/w \"NickName With Spaces\" Message -- send PM to NickName\n"
-      "/party Message -- send message to your party\n"
-      "/online -- show online players\n"
-      "/away [optional afk message] -- away from keyboard\n"
-      "/back -- you are back!\n"
-      "/sit -- Sit down\n"
-      "/stand -- Stand up\n"
-      "/turn left|right|up|down -- turn in given direction\n"
-      "/equip ID -- equip item id\n"
-      "/block PlayerName  -- block player\n"
-      "/dc -- disconnect\n"
-      "/debug -- toggle printing debug information\n"
-      "Any other command sends a message to the public chat"
-      )))
-   ((string-equal tmwchat-sent "/online")
-    (tmwchat-log (format "%s" (tmwchat-get-online-users))))
-   ((string-equal tmwchat-sent "/room")
-    (tmwchat-show-beings))
-   ((string-equal tmwchat-sent "/sit")
-    (tmwchat-sit))
-   ((string-equal tmwchat-sent "/stand")
-    (tmwchat-stand))
-   ((string-prefix-p "/turn " tmwchat-sent)
-    (tmwchat-turn (substring tmwchat-sent 6)))
-   ((string-prefix-p "/emote " tmwchat-sent)
-    (show-emote (string-to-int (substring tmwchat-sent 7))))
-   ((string-equal "/emotes" tmwchat-sent)
-    (tmwchat-log (format "%s" tmwchat-emotes)))
-   ((string-equal "/mute" tmwchat-sent)
-    (tmwchat-log "Sounds are muted")
-    (setq tmwchat-sound nil))
-   ((string-equal "/unmute" tmwchat-sent)
-    (tmwchat-log "Sounds are played")
-    (setq tmwchat-sound t))
-   ((string-prefix-p "/party " tmwchat-sent)
-    (tmwchat-send-party-message
-     (tmwchat--make-urls (substring tmwchat-sent 7))))
-   ((string-equal "/back" tmwchat-sent)
-    (setq tmwchat-away nil))
-   ((string-prefix-p "/away" tmwchat-sent)
-    (setq tmwchat-away t)
-    (condition-case nil
-	(let ((afk-msg (substring tmwchat-sent 6)))
-	  (unless (= (length afk-msg) 0)
-	    (setq tmwchat-away-message (concat "*AFK*: " afk-msg))))
-      (error nil))
-    (tmwchat-log tmwchat-away-message))
-   ((string-prefix-p "/equip " tmwchat-sent)
-    (tmwchat-equip-item (string-to-int (substring tmwchat-sent 7))))
-   ((string-prefix-p "/block " tmwchat-sent)
-    (let ((nick (chomp (substring tmwchat-sent 7))))
-      (when (string-prefix-p "\"" nick)
-	(setq nick (substring nick 1 (- (length nick) 1))))
-      (when (> (length nick) 0)
-	(tmwchat-log "Blocking player \"%s\"" nick)
-	(customize-set-value
-	 'tmwchat-blocked-players
-	 (add-to-list 'tmwchat-blocked-players nick)))))
-   ((string-equal "/dc" tmwchat-sent)
-    (tmwchat-logoff))
-   ((string-equal "/debug" tmwchat-sent)
-    (setq tmechat-debug (not tmwchat-debug)))
-   ((string-prefix-p "/w " tmwchat-sent)
-    (let* ((parsed (tmwchat--parse-msg (substring tmwchat-sent 3)))
-	   (nick (car parsed))
-	   (msg (tmwchat--make-urls (cdr parsed))))
-      (setq tmwchat--last-whisper-nick nick)
-      (whisper-message nick msg)))
-   ;; ((string-prefix-p "/ " tmwchat-sent)
-   ;;  (whisper-message
-   ;;   tmwchat--last-whisper-nick
-   ;;   (substring tmwchat-sent 2)))
-   ((string-prefix-p "/" tmwchat-sent)
-    (tmwchat-log "Unknown command. Type /help to get available commands"))
-   (t
-    (if tmwchat--whisper-target
-	(whisper-message tmwchat--whisper-target tmwchat-sent)
-      (progn
-	(setq tmwchat--last-whisper-nick nil)
-	(chat-message (tmwchat--make-urls tmwchat-sent))))))
-  (delete-region tmwchat--start-point (point-max))
-  ;; (setq tmwchat--start-point (point-max))
-  (when tmwchat--last-whisper-nick
-    (let ((nick-q (if (string-match-p " " tmwchat--last-whisper-nick)
-		      (concat "\"" tmwchat--last-whisper-nick "\"")
-		    tmwchat--last-whisper-nick)))
-      (insert (concat "/w " nick-q " "))
-      (goto-char (point-max)))))
-
-(defun tmwchat-set-online-users (users-list)
-  (cond
-   ((= tmwchat--online-list-number 0)
-    (setq tmwchat--online-list-number 1
-	  tmwchat--online-list-0 users-list))
-   ((= tmwchat--online-list-number 1)
-    (setq tmwchat--online-list-number 0
-	  tmwchat--online-list-1 users-list))))
-
-(defun tmwchat-get-online-users ()
-  (cond
-   ((= tmwchat--online-list-number 0)
-    tmwchat--online-list-1)
-   ((= tmwchat--online-list-number 1)
-    tmwchat--online-list-0)))
-
-(defun tmwchat--online-list ()
-  (defun chomp-end (str)
-    (when (string-suffix-p "(GM) " str)
-      (setq str (substring str 0 -5)))
-    (replace-regexp-in-string (rx (* (any " \t\n")) eos)
-			      ""
-			      str))
-  (defun gen-list (str)
-    (let* ((m (string-match "------------------------------" str))
-	   (end (+ (match-end 0) 1))
-	   (strm (substring str end))
-	   (m2 (string-match "\n\n" strm))
-	   (start (match-beginning 0))
-	   (strmm (substring strm 0 start)))
-      (mapcar 'chomp-end (split-string strmm "\n"))))
-  (defun callback (status)
-    (let ((onl)
-	  (data (buffer-string)))
-      (setq onl (gen-list data))
-      (tmwchat-set-online-users onl)
-      (setq tmwchat--speedbar-dirty t)
-      (kill-buffer (current-buffer))))
-  (let ((url "http://server.themanaworld.org/online.txt"))
-    (url-retrieve url 'callback nil t t)))
-
-(defun tmwchat-time ()
-  (let ((date (format-time-string "%D")))
-    (if (string-equal date tmwchat--date)
-	(format-time-string "%R")
-      (progn
-	(setq tmwchat--date date)
-	(format-time-string "%D %R")))))
-
-(defun tmwchat-redisplay-player-name (id name)
-  (with-current-buffer tmwchat-buffer-name
-    (save-excursion
-      (let ((inhibit-read-only t)
-	    (old-name (format "{{ID:%s}}" id)))
-	(replace-string old-name name nil (point-min) (point-max))
-	(goto-char (point-max))
-	(beginning-of-line)
-	(setq tmwchat--start-point (point))))))
 
 (provide 'tmwchat)
